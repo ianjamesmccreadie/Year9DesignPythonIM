df18fa63b5966553e39325ce271a8eab113f1515
Ian McCreadie <ian.mccreadie@GCCYJTMRJ1WK.local>
2019-11-29 14:51:13 -0500

    Saving To A .txt

--- Creating the solution/mvp.py
+++ Creating the solution/mvp.py
@@ -1,8 +1,43 @@
 import tkinter as tk #imports the Tkinter module, which is used to create your Graphical User Interface as "tk" for ease
 from PIL import ImageTk, Image #imports the Pillow (PIL) module, which is used to implement images into your tK window
+from tkinter import messagebox
+
 
 root = tk.Tk()
 root.configure(background='#5495f0') #sets the background color as defined by the hexcode
+
+dataFile = open("data.txt","w")
+
+#parallel arrays
+age = []
+submission = []#this is a list of names
+
+
+def runMe():
+  print("run me")
+  #STEP 1: STORE THE AGE
+  age.append(variable.get())
+  submission.append(e.get("1.0",tk.END))
+
+  print(age)
+  print(submission)
+def on_closing():
+  print("closing")
+  #STEP 2 - CREATE MESSAGEBOX
+  if messagebox.askokcancel("Quit", "Do you want to quit?"):
+
+    for i in range(0,len(age),1) :
+      dataFile.write(age[i]+"\n")
+      dataFile.write(submission[i]+"\n")
+
+
+    dataFile.close()
+    root.destroy()
+
+
+
+
+
 
 #creates the title using the label function
 
@@ -58,7 +93,7 @@ e.grid(row=2, column=0, columnspan=4)
 
 
 
-btn = tk.Button(text = "Submit", bd = 2,width =  20 ) # builds a button widget, specifcally for the submission button
+btn = tk.Button(text = "Submit", bd = 2,width =  20, command = runMe ) # builds a button widget, specifcally for the submission button
 btn.config()
 btn.grid(row=3, column=2)
 
@@ -102,15 +137,10 @@ w.config()
 w.grid(row=3, column=3, sticky = "SE" )
 
 
-root.mainloop()
-
-root = tk.Tk()
-
-title = tk.Label(text = "Thanks For Submitting!", font = ("Bebas", 40))
-title.config()
-title.grid(row = 0, column=1)
 
 
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
 
 
 root.mainloop()
